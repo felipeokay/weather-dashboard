a95c409e15fb38417c298faf211e0597f8ca3e81
unknown <kevinserrano0823@gmail.com>
2023-10-03 23:40:48 -0500

    README.md

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# weather-dashboard
+# Weather-Dashboard
+
+## Over-View
+This challenge was definetely a challenge. In this module I learned how to use access keys from third-party APIs and implement them into a useful application like a weather dashboard. Bootstrap was used in the HTML file with some hard coding in CSS. The star of the show is Javascript alongside with Jquery. Psuedo coding can be seen all over each file helping other developers if come across this repository. 
+
+![Alt text](weather.png)
+
+## Contribute 
+Kevinserrano0823@gmail.com
---
+++ script.js
@@ -0,0 +1,73 @@
+$(document).ready(function () {
+
+    const apiKey = '767baab1ba615005b7b57e268ed513fe';
+
+    // Selectors for HTML elements to display weather information
+    const cityEl = $('h2#city');
+    const dateEl = $('h3#date');
+    const weatherIconEl = $('img#weather-icon');
+    const temperatureEl = $('span#temperature');
+    const humidityEl = $('span#humidity');
+    const windEl = $('span#wind');
+    const uvIndexEl = $('span#uv-index');
+    const cityListEl = $('div.cityList');
+
+   // Selectors for form elements
+   const cityInput = $('#city-input');
+
+   // Store past searched cities
+   let pastCities = [];
+
+   function compare(a, b) {
+       const cityA = a.city.toUpperCase();
+       const cityB = b.city.toUpperCase();
+
+       let comparison = 0;
+       if (cityA > cityB) {
+           comparison = 1;
+       } else if (cityA < cityB) {
+           comparison = -1;
+       }
+       return comparison;
+   }
+
+    // Load events from local storage
+    function loadCities() {
+        const storedCities = JSON.parse(localStorage.getItem('pastCities'));
+        if (storedCities) {
+            pastCities = storedCities;
+        }
+    }
+
+    // Store searched cities in local storage
+    function storeCities() {
+        localStorage.setItem('pastCities', JSON.stringify(pastCities));
+    }
+
+   // Functions to build the URL for the OpenWeather API call
+ 
+    function buildURLFromInputs(city) {
+        if (city) {
+            return `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+        }
+    }
+
+    function buildURLFromId(id) {
+        return `https://api.openweathermap.org/data/2.5/weather?id=${id}&appid=${apiKey}`;
+    }
+
+     // Function to display the last 5 searched cities
+     function displayCities(pastCities) {
+        cityListEl.empty();
+        pastCities.splice(5);
+        let sortedCities = [...pastCities];
+        sortedCities.sort(compare);
+        sortedCities.forEach(function (location) {
+            let cityDiv = $('<div>').addClass('col-12 city');
+            let cityBtn = $('<button>').addClass('btn btn-light city-btn').text(location.city);
+            cityDiv.append(cityBtn);
+            cityListEl.append(cityDiv);
+        });
+    }
+    
+    
